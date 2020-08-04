8aae357b2b74ec4e1bd9c7324e2a1f375937b63e
Alexandra Collins <alexandracollins2022@gmail.com>
2020-08-04 18:35:53 -0500

    Initial Commit

---
+++ SAEtOApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SAEtOApp
+//
+//  Created by Ivette Collins on 8/4/20.
+//  Copyright © 2020 AlexandraCollins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
